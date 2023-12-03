4f9ae6605a3a5c20847bf8c7742d423509b5ea07
Bast-94 <bh.69@live.fr>
2023-12-03 15:21:28 +0100

    ADD naive search with plots.

--- max_clique.py
+++ max_clique.py
@@ -4,26 +4,13 @@ import random
 
 import matplotlib.pyplot as plt
 import networkx as nx
-
+from src.utils import clique, edges_list
 from src.bron_kerbosch import bron_kerbosch
 from src.bron_kerbosch_degeneracy import bron_kerbosch_degeneracy
 from src.bron_kerbosch_pivot import bron_kerbosch_pivot
-
+from src.naive_clique_search import naive_clique_search
+import time
 IMG_DIR = "img"
-
-
-def clique(n_clique):
-    for i in range(1, n_clique + 1):
-        for j in range(i + 1, n_clique + 1):
-            yield (i, j)
-
-
-def edges_list(n_clique, additional_nodes):
-    for i in range(n_clique + 1, n_clique + 1 + additional_nodes):
-        yield (
-            random.randint(0, n_clique + additional_nodes),
-            random.randint(0, n_clique + additional_nodes),
-        )
 
 
 if __name__ == "__main__":
@@ -38,11 +25,13 @@ if __name__ == "__main__":
     edges = list(edges_list(n_clique, additional_nodes))
 
     G.add_edges_from(edges + clique_edges)
-    bron_kerbosch_funcs = [bron_kerbosch, bron_kerbosch_pivot, bron_kerbosch_degeneracy]
+    bron_kerbosch_funcs = [bron_kerbosch, bron_kerbosch_pivot, bron_kerbosch_degeneracy, naive_clique_search]
     fig = plt.figure()
     for func in bron_kerbosch_funcs:
+        begin = time.time()
         max_clique = func(G)
-
+        end = time.time()
+        delta = end - begin
         cmap = lambda node: "red" if node in max_clique else "blue"
         node_color = list(map(cmap, G.nodes))
         pos = nx.spring_layout(G)
@@ -57,5 +46,5 @@ if __name__ == "__main__":
             font_size=10,
             ax=ax,
         )
-        ax.set_title(f"Graph with a {len(max_clique)} clique using {func.__name__}")
+        ax.set_title(f"Graph with a {len(max_clique)} clique using {func.__name__}, time: {delta:.2f}s")
         fig.savefig(os.path.join(IMG_DIR, f"{func.__name__}.png"))
